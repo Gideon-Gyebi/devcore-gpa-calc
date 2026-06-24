15e78adcda521bfbf62c23e7c45ed5ebb4758212
Gideon-Gyebi <gyebigideon000@gmail.com>
2026-06-24 21:04:54 +0000

    Conversion function.

---
+++ src/gpconv.c
@@ -0,0 +1,19 @@
+#include "gpa.h"
+
+char *gpconv(int a)
+{
+    int score;
+
+    if (score >= 85)
+    {
+        return "A+";
+    }
+    else if (score >= 80)
+    {
+        return "A";
+    }
+    else (score >= 75);
+    
+    return "B+";
+    
+}
