1fd7b4e9d1afc0a40fc076b7cc17bc871c5c3d08
Gideon-Gyebi <gyebigideon000@gmail.com>
2026-07-08 20:51:50 +0000

    Refactor comments in gpa.h for improved clarity and formatting

--- include/gpa.h
+++ include/gpa.h
@@ -1,11 +1,19 @@
-#ifndef GPA_H // It prevents the contents of the 'GPA_H' from being processed multiple times if it has
-// been included in the header file
-#define GPA_H // Defines the 'GPA_H' macro. It prevents any future '#ifndef GPA_H' from running to avoid
-// duplicate definition errors.
+/*
+It prevents the contents of the 'GPA_H' from being processed 
+multiple times if it has been included in the header file
+*/ 
+#ifndef GPA_H 
+/*
+Defines the 'GPA_H' macro. It prevents any future '#ifndef GPA_H' 
+from running to avoid duplicate definition errors.
+*/
+#define GPA_H
 
-char *gpconv(int a); // Declares the function name 'gpconv.c' and tells the compiler that this function
-                     // accepts an integer (int a) as an input and returns a pointer to a character string (char *).
+char *gpconv(int a); // Function prototype for 'gpconv'. Returns a pointer to a character string.
 
-#endif // Closes the '#ifndef' conditional block and ends the definition of the 'GPA_H' macro. It ensures
-// that the contents of the header file are only included once during compilation, preventing duplicate
-// definition errors.
+/*
+Closes the '#ifndef' conditional block and ends the definition of the 
+'GPA_H' macro. It ensures that the contents of the header file are only
+included once during compilation, preventing duplicate definition errors.
+*/
+#endif 
