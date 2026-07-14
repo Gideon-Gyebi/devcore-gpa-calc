cd5086f744f7e5fd071d13c0e051216391434dd0
Amarteifio Jormo <amarteifiojormo@gmail.com>
2026-07-13 21:22:39 -0700

    added lgconv function prototype

--- include/gpa.h
+++ include/gpa.h
@@ -1,21 +1,21 @@
 /*
-It prevents the contents of the 'GPA_H' from being processed 
+It prevents the contents of the 'GPA_H' from being processed
 multiple times if it has been included in the header file
-*/ 
-#ifndef GPA_H 
+*/
+#ifndef GPA_H
 /*
-Defines the 'GPA_H' macro. It prevents any future '#ifndef GPA_H' 
+Defines the 'GPA_H' macro. It prevents any future '#ifndef GPA_H'
 from running to avoid duplicate definition errors.
 */
 #define GPA_H
 
-float getch(void); // Function prototype for 'getch'. Returns a float value representing the number of credit hours.
-char *scrconv(int score); // Function prototype for 'scrconv'. Returns a pointer to a character string.
+float getch(void);                                 // Function prototype for 'getch'. Returns a float value representing the number of credit hours.
+char *scrconv(int score);                          // Function prototype for 'scrconv'. Returns a pointer to a character string.
 float gpcalc(float gradeValue, float creditHours); // Function prototype for 'gpcalc'. Returns a float grade value.
-
+float lgconv(char *letterGrade);
 /*
-Closes the '#ifndef' conditional block and ends the definition of the 
+Closes the '#ifndef' conditional block and ends the definition of the
 'GPA_H' macro. It ensures that the contents of the header file are only
 included once during compilation, preventing duplicate definition errors.
 */
-#endif 
+#endif
