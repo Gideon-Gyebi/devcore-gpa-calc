341790d6210efef583974b4a36ffbc593b817fc3
Amarteifio Jormo <amarteifiojormo@gmail.com>
2026-07-13 21:57:17 -0700

    Fix comparison logic in lgconv function for letter grades

--- src/lgconv.c
+++ src/lgconv.c
@@ -1,44 +1,46 @@
 #include "gpa.h"
+#include <string.h>
 
 float lgconv(char *letterGrade) // Function to convert a lettergrade to a grade value
 {
     // Determine the grade value based on its corresponding letter grade
-    if (letterGrade = "A+")
+    // if (letterGrade = "A+")
+    if (strcmp(letterGrade, "A+") == 0)
     {
         return 5.0;
     }
 
-    else if (letterGrade = "A")
+    else if (strcmp(letterGrade, "A") == 0)
     {
         return 4.5;
     }
 
-    else if (letterGrade = "B+")
+    else if (strcmp(letterGrade, "B+") == 0)
     {
         return 4.0;
     }
 
-    else if (letterGrade = "B")
+    else if (strcmp(letterGrade, "B") == 0)
     {
         return 3.5;
     }
 
-    else if (letterGrade = "C+")
+    else if (strcmp(letterGrade, "C+") == 0)
     {
         return 3.0;
     }
 
-    else if (letterGrade = "C")
+    else if (strcmp(letterGrade, "C") == 0)
     {
         return 2.5;
     }
 
-    else if (letterGrade = "D+")
+    else if (strcmp(letterGrade, "D+") == 0)
     {
         return 2.0;
     }
 
-    else if (letterGrade = "D")
+    else if (strcmp(letterGrade, "D") == 0)
     {
         return 1.5;
     }
