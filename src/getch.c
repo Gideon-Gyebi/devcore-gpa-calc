5896dff10c696f3d28a0a2cb80a671e25224ee92
Gideon-Gyebi <gyebigideon000@gmail.com>
2026-07-17 20:08:55 +0000

    Improve error messages in getch function for credit hours input validation

--- src/getch.c
+++ src/getch.c
@@ -7,14 +7,16 @@ float getch(void)
     float creditHours;
 
     printf("Enter the number of credit hours (e.g., 3.0 or 3): ");
+    
     if (scanf("%f", &creditHours) != 1) // Check if the input is valid
     {
-        fprintf(stderr, "Error reading credit hours.\n"); // Print an error message to standard error
+        fprintf(stderr, "GETCH_Error: Invalid input for credit hours.\n"); // Print an error message to standard error
         return -1; // Return an error value
     }
+
     if (creditHours < 1 || creditHours > 3 ) // Check if the credit hours are within the valid range (1 to 3)
     {
-        fprintf(stderr, "Credit hours must be between 1 and 3.\n"); // Print an error message to standard error
+        fprintf(stderr, "GETCH_Error: Credit hours must be between 1 and 3.\n"); // Print an error message to standard error
         return -1; // Return an error value
     }
 
