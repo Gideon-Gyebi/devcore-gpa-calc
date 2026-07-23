84b0710049fbddcb0d2d17b34e61104ec3864f4b
Amarteifio Jormo <amarteifiojormo@gmail.com>
2026-07-22 21:15:31 -0700

    changed the ERROR message

--- src/getch.c
+++ src/getch.c
@@ -2,25 +2,26 @@
 #include "gpa.h" // Include the header file for GPA calculation
 
 // Function to read the number of credit hours from the user
-float getch(void) 
+float getch(void)
 {
     float creditHours;
 
     printf("Enter the number of credit hours (e.g., 3.0 or 3): ");
-    
+
     if (scanf("%f", &creditHours) != 1) // Check if the input is valid
     {
         fprintf(stderr, "GETCH_Error: Invalid input for credit hours.\n"); // Print an error message to standard error
-        return -1; // Return an error value
+        return -1;                                                         // Return an error value
     }
 
-    if (creditHours < 1 || creditHours > 3 ) // Check if the credit hours are within the valid range (1 to 3)
+    if (creditHours < 1 || creditHours > 3) // Check if the credit hours are within the valid range (1 to 3)
     {
-        fprintf(stderr, "GETCH_Error: Credit hours must be between 1 and 3.\n"); // Print an error message to standard error
-        return -1; // Return an error value
+        fprintf(stderr, "GETCH_Error: Credit hours should range from 1 and 3.\n"); // Print an error message to standard error
+        return -1;                                                                 // Return an error value
     }
 
-    while (getchar() != '\n'); // Clear the input buffer to remove any leftover characters
+    while (getchar() != '\n')
+        ; // Clear the input buffer to remove any leftover characters
 
     return creditHours;
 }
