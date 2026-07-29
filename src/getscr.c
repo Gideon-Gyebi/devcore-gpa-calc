0210b59aba1655fbb8c4c76a4082db6f9a4d517f
Gideon-Gyebi <gyebigideon000@gmail.com>
2026-07-29 21:15:21 +0000

    Refactor getscr function by adding a loop.

--- src/getscr.c
+++ src/getscr.c
@@ -5,22 +5,33 @@
 int getscr(void)
 {
     int score;
+    char clear; // Variable to clear the input buffer
 
-    printf("Enter the score (e.g., 85 or 90): ");
+    do
+    {
+        printf("Enter the score (e.g., 85 or 90): ");
+        
+        if (scanf("%d", &score) != 1) // Check if the input is valid
+        {
+            fprintf(stderr, "GETSCR_Error: Invalid input for score.\n"); // Print an error message to standard error
+        
+            while (getchar() != '\n'); // Clear the input buffer to remove any leftover characters
+        }
+
+        else if (score < 0 || score > 100) // Check if the score is within the valid range (0 to 100)
+        {
+            fprintf(stderr, "GETSCR_Error: Score must be between 0 and 100.\n"); // Print an error message to standard error
+        
+            while (getchar() != '\n'); // Clear the input buffer to remove any leftover characters
+        }
+
+        else if (scanf("%c", &clear) == 1 && clear != '\n') // Check if there are any leftover characters in the input buffer
+        {
+            fprintf(stderr, "GETSCR_Warning: Extra characters are not allowed.\n"); // Print an error message to standard error
+            
+            while (getchar() != '\n'); // Clear the input buffer to remove any leftover characters
+        }
+    } while (score < 0 || score > 100); // Repeat the prompt until a valid score is entered
     
-    if (scanf("%d", &score) != 1) // Check if the input is valid
-    {
-        fprintf(stderr, "GETSCR_Error: Invalid input for score.\n"); // Print an error message to standard error
-        return -1; // Return an error value
-    }
-
-    if (score < 0 || score > 100 ) // Check if the score is within the valid range (0 to 100)
-    {
-        fprintf(stderr, "GETSCR_Error: Score must be between 0 and 100.\n"); // Print an error message to standard error
-        return -1; // Return an error value
-    }
-
-    while (getchar() != '\n'); // Clear the input buffer to remove any leftover characters
-
     return score;
 }
