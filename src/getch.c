3a5672036978fbd0108169bc0033f4d952b8073d
Amarteifio Jormo <amarteifiojormo@gmail.com>
2026-07-29 21:37:37 -0700

    Refactor getch function to improve input validation and error handling for credit hours

--- src/getch.c
+++ src/getch.c
@@ -5,23 +5,33 @@
 float getch(void)
 {
     float creditHours;
+    char clear; // Variable to clear the input buffer
 
-    printf("Enter the number of credit hours (e.g., 3.0 or 3): ");
-
-    if (scanf("%f", &creditHours) != 1) // Check if the input is valid
+    do
     {
-        fprintf(stderr, "GETCH_Error: Invalid input for credit hours.\n"); // Print an error message to standard error
-        return -1;                                                         // Return an error value
-    }
+        printf("Enter the number of credit hours (e.g., 3.0 or 3): ");
 
-    if (creditHours < 1 || creditHours > 3) // Check if the credit hours are within the valid range (1 to 3)
-    {
-        fprintf(stderr, "GETCH_Error: Credit hours should range from 1 and 3.\n"); // Print an error message to standard error
-        return -1;                                                                 // Return an error value
-    }
+        if (scanf("%f", &creditHours) != 1) // Check if the input is valid
+        {
+            fprintf(stderr, "GETCH_Error: Invalid input for credit hours.\n"); // Print an error message to standard error
+            while (getchar() != '\n')
+                ; // Clear the input buffer to remove any leftover characters
+        }
 
-    while (getchar() != '\n')
-        ; // Clear the input buffer to remove any leftover characters
+        else if (creditHours < 1 || creditHours > 3) // Check if the credit hours are within the valid range (1 to 3)
+        {
+            fprintf(stderr, "GETCH_Error: Credit hours should range from 1 and 3.\n"); // Print an error message to standard error
+            while (getchar() != '\n')
+                ; // Clear the input buffer to remove any leftover characters
+        }
+
+        else if (scanf("%c", &clear) == 1 && clear != '\n') // Check if there are any leftover characters in the input buffer
+        {
+            fprintf(stderr, "GETCH_Warning: Extra characters are not allowed.\n"); // Print an error message to standard error
+            while (getchar() != '\n')
+                ; // Clear the input buffer to remove any leftover characters
+        }
+    } while (creditHours < 1 || creditHours > 3); // Repeat the prompt until valid credit hours are entered
 
     return creditHours;
 }
