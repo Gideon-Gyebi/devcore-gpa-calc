c5c200fe629e678e6a69336d108fdec3fe0fe2a0
Amarteifio Jormo <amarteifiojormo@gmail.com>
2026-08-04 17:56:22 -0700

    Refactor function prototypes in gpa.h for clarity and consistency

--- include/gpa.h
+++ include/gpa.h
@@ -20,17 +20,16 @@ typedef struct
 
 } GpaRecord;
 
-
 void savechgp(GpaRecord gpaStorage[], int index, float creditHour, float gradePoint); // Function prototype for saving credit hour and grade point
 
-int getnumcrs(void);                               // Function prototype for 'getnumcrs'. Returns an integer value representing the number of courses.
-char *getcrs(void);                                // Function prototype for 'getcrs'. Returns a pointer to a character string representing the course name.
-float getch(void);                                 // Function prototype for 'getch'. Returns a float value representing the number of credit hours.
-int getscr(void);                                  // Function prototype for 'getscr'. Returns an integer value representing the score.
-char *scrconv(int score);                          // Function prototype for 'scrconv'. Returns a pointer to a character string.
-float gpcalc(float gradeValue, float creditHours); // Function prototype for 'gpcalc'. Returns a float grade value.
-float lgconv(char *letterGrade);                   // Function prototype for 'lgconv'. Returns a float value representing the letter grade.
-float tgpcalc(float gradePoint, float gradeValue);
+int getnumcrs(void);                                   // Function prototype for 'getnumcrs'. Returns an integer value representing the number of courses.
+char *getcrs(void);                                    // Function prototype for 'getcrs'. Returns a pointer to a character string representing the course name.
+float getch(void);                                     // Function prototype for 'getch'. Returns a float value representing the number of credit hours.
+int getscr(void);                                      // Function prototype for 'getscr'. Returns an integer value representing the score.
+char *scrconv(int score);                              // Function prototype for 'scrconv'. Returns a pointer to a character string.
+float gpcalc(float gradeValue, float creditHours);     // Function prototype for 'gpcalc'. Returns a float grade value.
+float lgconv(char *letterGrade);                       // Function prototype for 'lgconv'. Returns a float value representing the letter grade.
+float tgpcalc(GpaRecord gpaStorage[], int numCourses); // Function prototype for 'tgpcalc'. Returns a float value representing the total grade points of all stored courses.
 float tchcalc(GpaRecord gpaStorage[], int numCourses); // Function prototype for 'tchcalc'. Returns a float value representing the total credit hours of all stored courses.
 
 /*
