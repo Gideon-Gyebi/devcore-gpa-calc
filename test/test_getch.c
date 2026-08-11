aee6150f5f30dc8a23225728f80def2d1a573b3f
Gideon-Gyebi <gyebigideon000@gmail.com>
2026-08-11 22:47:54 +0000

    Refactor test_getch function and expand test cases for improved validation

--- test/test_getch.c
+++ test/test_getch.c
@@ -17,34 +17,96 @@
  */
 
 
-void test_getch(void)
+float test_getch(void)
  {
      float creditHours = getch();
-     printf("Credit Hours Entered: %.2f\n", creditHours);
+
+     return creditHours;
  }
 
 
 int main()
 {
-    printf("===== GETCH Function Tests =====\n\n");
+    float creditHours;
 
-    printf("Test Case 1: Valid input (1) Lower Boundary\n");
+    printf("\n===================== GETCH Function Tests =====================\n\n");
+
+    printf("Test Case 1: Valid input (1) Lower Boundary.\n");
+    creditHours = test_getch();
+    if (creditHours == 1.0f)
+    {
+        printf("\n[Credit Hours Entered: %.1f]\n", creditHours);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("----------------------------------------------------------------\n");
+
+    printf("\nTest Case 2: Valid input (3) Upper Boundary.\n");
+    creditHours = test_getch();
+    if (creditHours == 3.0f)
+    {
+        printf("\n[Credit Hours Entered: %.1f]\n", creditHours);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("----------------------------------------------------------------\n");
+
+    printf("\nTest Case 3: Valid input (2) Normal Value.\n");
+    creditHours = test_getch();
+    if (creditHours == 2.0f)
+    {
+        printf("\n[Credit Hours Entered: %.1f]\n", creditHours);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("----------------------------------------------------------------\n");
+
+    printf("\nTest Case 4: Invalid input (2.5) Valid floating-point input.\n");
+    creditHours = test_getch();
+    if (creditHours == 2.5f)
+    {
+        printf("\n[Credit Hours Entered: %.1f]\n", creditHours);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+    
+    printf("----------------------------------------------------------------\n");
+    
+    printf("\nTest Case 5: Invalid input (2abc) Valid + Invalid Characters.\n");
+    creditHours = test_getch();
+    if (creditHours == 2.0f)
+    {
+        printf("\n[Credit Hours Entered: %.1f]\n", creditHours);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("----------------------------------------------------------------\n");
+
+    printf("\nTest Case 6: Invalid input samples (0, 4, abc) Below Range,\nAbove Range and Completely Invalid.\n");
     test_getch();
-
-    printf("\nTest Case 2: Valid input (3) Upper Boundary\n");
-    test_getch();
-
-    printf("\nTest Case 3: Valid input (2) Normal Value\n");
-    test_getch();
-
-    printf("\nTest Case 4: Invalid input (2.5) Valid floating-point input\n");
-    test_getch();
-
-    printf("\nTest Case 5: Invalid input (2abc) Valid + Invalid Characters\n");
-    test_getch();
-
-    printf("\nTest Case 6: Invalid input samples (0, 4, abc) Below Range\n");
-    test_getch();
-
+    printf("\n[PASS] = Two range errors and one invalid input error.\n");
+    printf("[FAIL] = If there are less than three error messages.\n");
+    
+    printf("\n=========================== Test End ===========================\n");
+    
     return 0;
 }
