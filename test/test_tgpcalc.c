2d01b2eacf3d2c1b891910137e23ddabc50b5ec3
Amarteifio Jormo <amarteifiojormo@gmail.com>
2026-08-28 17:21:21 -0700

    Refactor test cases in test_tgpcalc.c for clarity and consistency

--- test/test_tgpcalc.c
+++ test/test_tgpcalc.c
@@ -22,14 +22,14 @@ int main()
     float totalGradePoints;
     int numCourses;
 
-    printf("\n===================== TGPCALC Function Tests =====================\n\n");
+    printf("\n===================TGPCALC Function Tests===================\n\n");
 
-    printf("Test Case 1: Valid Input (3 courses with gradepoints 12.0, 4.5, 6.0) Normal Case.\n");
+    printf("Test Case 1: Valid Input (3 courses with grade points 12.0, 4.5, 6.0) Normal Case.\n");
     GpaRecord gpaStorage[3] =
         {
-            {.gradePoint = 12.0f},
-            {.gradePoint = 4.5f},
-            {.gradePoint = 6.0f}};
+            {.gradePoint = 12.0},
+            {.gradePoint = 4.5},
+            {.gradePoint = 6.0}};
     numCourses = 3;
     totalGradePoints = test_tgpcalc(gpaStorage, numCourses);
     if (totalGradePoints == 22.5f)
@@ -41,3 +41,46 @@ int main()
     {
         printf("[FAIL]\n");
     }
+
+    printf("--------------------------------------------------\n\n");
+
+    printf("Test Case 2: Valid Input (1 course with grade point 12.0) Single Course.\n");
+    GpaRecord gpaStorage2[1] =
+        {
+            {.gradePoint = 12.0f}};
+    numCourses = 1;
+    totalGradePoints = test_tgpcalc(gpaStorage2, numCourses);
+    if (totalGradePoints == 12.0f)
+    {
+        printf("\n[Total Grade Points Calculated: %.1f]\n", totalGradePoints);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("----------------------------------------------------\n\n");
+
+    // An array named gpaStorag3 with space for 1 GpaRecord
+    GpaRecord gpaStorage3[1] =
+        {
+            // Set the gradePoint of the single record to 0.0f
+            {.gradePoint = 0.0f}};
+    numCourses = 0;
+
+    totalGradePoints = test_tgpcalc(gpaStorage3, numCourses);
+    if (totalGradePoints == 0.0f)
+    {
+        printf("\n[Total Grade Points Calculated: %.1f]\n", totalGradePoints);
+        printf("[PASS]\n");
+    }
+    else
+    {
+        printf("[FAIL]\n");
+    }
+
+    printf("\n================================== Test End ==================================\n");
+
+    return 0;
+}
